a51e6689667779b947583f1668164b54592d654c
mos <david.simonnard@protonmail.com>
2023-03-11 16:25:41 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,56 @@
-const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
+const { Client, Events, GatewayIntentBits, ActivityType } = require('discord.js');
+const { createAudioPlayer, createAudioResource , StreamType, demuxProbe, joinVoiceChannel, NoSubscriberBehavior, AudioPlayerStatus, VoiceConnectionStatus, getVoiceConnection } = require('@discordjs/voice')
 const fetch = require('node-fetch');
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions,   GatewayIntentBits.GuildVoiceStates],
+});
 
 client.on('ready', () => {
-	console.log(`Logged in as ${client.user.tag}!`);
-	client.user.setPresence({
-	    status: 'dnd',
-	});
+	function presence () {
+		client.user.setPresence({ status: 'dnd' });
+		client.user.setActivity(`${client.guilds.cache.size} servers`, { type: ActivityType.Watching });
+	}
+
+	function radiostream () {
+		const guild = client.guilds.cache.get('664826892867862548');
+		const voiceChannel = guild.channels.cache.get('1015192386013507614');
+
+		const connection = joinVoiceChannel({
+			channelId: voiceChannel.id,
+			guildId: voiceChannel.guild.id,
+			adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+		})
+
+		const resource = createAudioResource('https://streams.radio.co/s1df61e0af/listen')
+
+		const player = createAudioPlayer()
+
+		player.play(resource)
+
+		player.on('error', error => {
+			radiostream();
+		});
+
+		player.on(AudioPlayerStatus.Idle, () => {
+			radiostream();
+		});
+
+		connection.subscribe(player)
+
+		connection.on('stateChange', (old_state, new_state) => {
+			if (old_state.status === VoiceConnectionStatus.Ready && new_state.status === VoiceConnectionStatus.Connecting) {
+				connection.configureNetworking();
+			}
+		})
+	}
+
+	radiostream();
+
+	presence();
+	setTimeout(() => {
+		presence();
+	}, 60000);
+
 });
 
 client.on('interactionCreate', async (interaction) => {
@@ -23,6 +67,16 @@ client.on('interactionCreate', async (interaction) => {
 			return interaction.reply('God damn it, we\'re out of big cats');
 		}
 
+	}
+});
+
+client.on('messageCreate', async (message) => {
+	const { channel } = message
+
+	if (message.guild && message.guild.id === '1081152759929245726') {
+		if (message.channel.id === '1082606297507168277') {
+			message.crosspost()
+		}
 	}
 });
 
