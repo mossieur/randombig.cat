bf6b9ba1fecea6c3b461296999301df25c9582b5
mos <david.simonnard@protonmail.com>
2023-03-12 14:34:07 +0100

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+<meta charset="utf-8">
+<title>Error</title>
+</head>
+<body>
+<pre>Cannot GET <?php echo $_SERVER['REQUEST_URI']; ?></pre>
+</body>
+</html>
